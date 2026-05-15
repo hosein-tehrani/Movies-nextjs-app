741aeae2bddf26bb3863934ac613b905503773af
Hosein-tehrani <ho3einvictorho3ein@gmail.com>
2026-05-15 18:17:03 +0330

    make movie cards component

---
+++ components/movie/MovieCard.jsx
@@ -0,0 +1,38 @@
+import {
+  Badge,
+  Card,
+  CardBody,
+  CardFooter,
+  CardImg,
+  CardLink,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+} from "react-bootstrap";
+import styles from "./MovieCard.module.css";
+import { BsStar, BsStarFill } from "react-icons/bs";
+import CardStar from "./CardStar";
+export default function MovieCard({ movie }) {
+  return (
+    <Card className={styles.footer}>
+      <CardBody>
+        <CardTitle>{movie.title}</CardTitle>
+        <CardSubtitle className="mb-2 text-muted">{movie.year}</CardSubtitle>
+        <CardImg variant="top" src={movie.poster}></CardImg>
+        {movie.genres.map((g, index) => (
+          <>
+            <Badge key={index} pill variant="secondary">
+              {g}
+            </Badge>
+          </>
+        ))}
+        <CardFooter className={styles.footer}>
+          <CardLink href={"/movies/" + movie.id}>details</CardLink>
+          <div>
+            <CardStar movie={movie}/>
+          </div>
+        </CardFooter>
+      </CardBody>
+    </Card>
+  );
+}
